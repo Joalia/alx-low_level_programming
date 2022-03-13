e683b84f4d6a1f6aecfb2b18556cd8d50cf81883
root <ail>
2022-03-12 23:36:50 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,6 +3,6 @@
 int main(void)
 {
 printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\n", sizeof(char), sizeof(int));
-printf("Size of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\n", sizeof(signed int), sizeof(unsigned int));
-printf("Size of a float: %d byte(s)", sizeof(float));
+printf("Size of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\n", sizeof(signed int), sizeof(long long int));
+printf("Size of a float: %d byte(s) ", sizeof(float));
 }
