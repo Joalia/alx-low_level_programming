817fc9f507c1b4b94a0d3426e21abab73a3582c8
root <ail>
2022-03-12 22:46:25 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+/* Print the text
+ *
+ */
 
-int main(void) {
-
+int main(void)
+{
 /* Print the text */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
