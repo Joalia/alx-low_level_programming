e24d84bcbfcaddd992cb705aa9811e70e2f6ae4a
root <ail>
2022-03-14 02:07:31 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
+/* more headers goes there */
 /**
 *main Entry point
+*
 *Return: 0
-**/
+*/
 int main(void)
 {
-/* Use printf function */
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
