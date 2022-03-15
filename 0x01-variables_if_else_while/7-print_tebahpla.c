d643da921c90561bbe6b49ce54c7c7a9bb15743e
root <ail>
2022-03-15 04:32:54 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Aways 0 (Success)
+ */
+int main(void)
+{
+        int n;
+	char z = 'z';
+
+	for (n == 26;n > 0; n--)
+	{
+		putchar(z);
+        }
+	putchar('\n');
+	return (0);
+}
