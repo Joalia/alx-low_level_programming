92bd1f4a72cf4f50e68edb5d5f11c142cb6633f2
root <ail>
2022-03-15 05:17:04 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Aways 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char ch = 'a';
+
+	for (n = 0; n < 16; n++)
+	{
+		if (n < 10)
+			putchar(n + '0');
+		if (n >= 10)
+			putchar(ch + 1);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
