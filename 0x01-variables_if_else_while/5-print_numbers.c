28f9a379f9f2770be73ea1f293d5ae9b1faff592
root <ail>
2022-03-14 20:58:22 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Aways 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 9)
+        {
+		printf(n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
