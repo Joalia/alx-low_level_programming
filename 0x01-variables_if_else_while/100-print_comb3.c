2803a15b3da27fbf47804e4d9489ba57ba39999a
root <ail>
2022-03-15 20:28:07 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,8 +18,8 @@ int main(void)
 			if (n != c)
 			{
 				putchar(48 + c);
-				putchar(48 +n);
-				if (n != 9 && c != 9)
+				putchar(48 + n);
+				if (n != 9 && c != 8)
 				{
 					putchar(',');
 					putchar(' ');
