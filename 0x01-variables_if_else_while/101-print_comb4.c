425f560d7ee3bf915773ecf569ed1d7bfcea1e72
root <ail>
2022-03-16 06:50:48 -0700

    101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * print three differents digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int f = 0; /* f is the first digits*/
+	int s; /* s is the second digit */
+	int t; /* t for the thirst digit*/
+
+	while (f < 10)
+        {
+                for (s = f; s < 10; s++)
+                {
+                        if (s != f)
+                        {
+				if (t != s)
+				{
+					putchar(48 + f);
+					putchar(48 + s);
+					putchar(48 + t);
+					if (f != 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+		f++;
+	}
+	putchar('\n');
+	return (0);
+}
