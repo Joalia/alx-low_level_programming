60ff6986b2fb73305bd186a1b697b130a435cf9a
root <ail>
2022-03-16 23:34:53 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,24 +5,25 @@
  * Using function's header
  * Return: Always 0 (Success)
  */
-void print_alphabet(void);
+void print_alphabet(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+	putchar('\n');
+}
 
 int main(void)
 {
 	print_alphabet();
 	return (0);
-}
-
-void print_alphabet()
-{
-	char a = 'a';
-	int n = 0;
-
-	while (n < 26)
-	{
-		putchar(a);
-		a++;
-		n++;
-	}
-	putchar('\n');
 }
