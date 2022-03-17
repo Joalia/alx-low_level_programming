87462eec1be2bf47a179a223ae1f4ef0311aca68
root <ail>
2022-03-17 01:10:05 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alphanet in lowercase  times followed by a new line
+ */
+
+void print_alphabetx(void)
+{
+	char letter;
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+	}
+	_putchar('\n');
+}
