bd3c1a7d024ec8ccc733c7948f5907567e88d1d0
root <ail>
2022-03-17 00:43:46 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,15 @@
 #include "main.h"
 #include <stdio.h>
+#include <unistd.h>
 /**
  * print_alphabet - prints the alphabet un lowercase, followed by a new line
  */
-
+int _putchar(char c)
+{
+	int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 void print_alphabet(void)
 {
 	char letter;
