1beb23d26cf6fc3bcbb037f60377f46935301acd
root <ail>
2022-03-17 10:50:51 -0700

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _islower - cchecks if is lowercase
+ * @n: The number to be checked
+ * Return: 1 when positive 0 when negative -1 else
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+\n");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+		printf("-\n");
+		return (-1);
+	}
+}
