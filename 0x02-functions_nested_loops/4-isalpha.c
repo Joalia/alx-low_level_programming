250e18aa55b71a68d15c4ca2e6ba128666c1f44a
root <ail>
2022-03-17 10:21:13 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,8 +7,9 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') && (c >= 'A' && c <= 'Z'))
+	if (c >= 'a' && c <= 'z')
 		return (1);
-	else
+	else if (c >= 'A' && c <= 'Z')
 		return (0);
+	return (0);
 }
