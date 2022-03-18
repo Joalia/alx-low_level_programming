37766c962b2f204176f0140ba42ba546630197ac
root <ail>
2022-03-18 05:11:15 -0700

    7-print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,11 +11,9 @@ int print_last_digit(int n)
 {
 	int last;
 
-	if (n >= 0)
-		n = n;
-	if (n < 0)
-		n = -n;
 	last = n % 10;
+	if (last < 0)
+		last = -last;
 	_putchar(last + '0');
 	return (last);
 }
