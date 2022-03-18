5fc1d4eb49228554ddea8260f0e40683d6a079b6
root <ail>
2022-03-18 04:44:29 -0700

    7-print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,12 +3,15 @@
 
 /**
  * print_last_digit - print the last digit of a number
- * @n: the number 
+ * @n: the number
  * Return: the value of the las digit
  */
 
 int print_last_digit(int n)
 {
-	printf("%d", n % 10);
-	return (n % 10);
+	int last;
+
+	last = _abs(n) % 10;
+	printf("%d", last);
+	return (last);
 }
