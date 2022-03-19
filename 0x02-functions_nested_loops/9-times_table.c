0c524971c7a8a90f597459dedd03662ea4fe5919
root <ail>
2022-03-18 20:54:40 -0700

    times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,27 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+ * times_table - prints the times table
+ *
+ */
+void times_table(void)
+{
+	int a, b;
 
+	for (a = 0; a < 10; a++)
+	{
+		_putchar('0');
+		_putchar(',');
 
-
-
-
+		for (b = 1; b < 10; b++)
+		{
+			_putchar(' ');
+			if (a * b / 10 == 0)
+				-putchar(' ');
+			printf("%d", a * b);
+			if (b < 9)
+				_putchar(',');
+		}
+	}
+}
