417cec8c8f13859a4331cafb488dcf50c084b22f
root <ail>
2022-03-19 22:38:43 -0700

    101-natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, som;
+	for (i = 0; i < 1025; i++)
+	{
+		if ( i % 3 = 0)
+			som = som + i;
+		else if ( i % 5)
+			som = som +i;
+	}
+	printf("%d\n", som);
+	return (0);
+}
