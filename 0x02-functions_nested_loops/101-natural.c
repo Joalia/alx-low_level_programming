53055aec76d186f8acdfaba5e1f3ff058c198266
root <ail>
2022-03-19 22:39:58 -0700

    101-natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,9 +9,9 @@ int main(void)
 	int i, som;
 	for (i = 0; i < 1025; i++)
 	{
-		if ( i % 3 = 0)
+		if ( i % 3 == 0)
 			som = som + i;
-		else if ( i % 5)
+		else if ( i % 5 == 0)
 			som = som +i;
 	}
 	printf("%d\n", som);
