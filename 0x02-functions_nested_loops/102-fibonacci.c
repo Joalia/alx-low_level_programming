c9dd239d2571d9d6b4524e534e42873e541ac08b
root <ail>
2022-03-20 10:47:52 -0700

    102-fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,14 +11,22 @@ int main(void)
 	unsigned long pp = 1;
 
 	printf("%lu, ", pp);
-	for (i = 0; i < 49; i++)
+	for (i = 0; i < 42; i++)
 	{
 		printf("%lu", p);
-		if (i < 48)
+		if (i < 42)
 			printf(", ");
 		act = p + pp;
 		pp = p;
 		p = act;
+	}
+	for (i = 0; i < 8; i++)
+	{
+		act = p + pp;
+		pp = p;
+		p = act;
+		if (i < 7)
+			printf(", ");
 	}
 	return (0);
 }
