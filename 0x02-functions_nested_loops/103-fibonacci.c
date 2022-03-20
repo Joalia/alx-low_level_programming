a1b376daa2f00ab180d9ec8897e313b406d3b420
root <ail>
2022-03-20 13:08:31 -0700

    103-fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,10 +6,9 @@
  */
 int main(void)
 {
-	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum = 0, sum2 = 0;
 
-	for (count = 0; count < 49; count++)
+	for (sum <= 4000000 )
 	{
 		sum = fib1 + fib2;
 		if (sum % 2 == 0)
