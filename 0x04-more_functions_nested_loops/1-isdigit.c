27045fd94b9fd2863f5dda7d8d050432b394106f
root <ail>
2022-03-21 15:38:30 -0700

    1-isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isgigit - if a number is a digit
+ * @c :  the number
+ *
+ * Return: 0 if false and 1 if true
+ */
+int _isdigit(int c)
+{
+	if (c % 10 != 0)
+		return (1);
+	else
+		return (0);
+}
