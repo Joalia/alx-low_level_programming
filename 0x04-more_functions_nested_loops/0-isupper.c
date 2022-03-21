cc4f7c337a1b594898e397edd12811346fdd84b9
root <ail>
2022-03-21 15:25:38 -0700

    0-

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <ctype.h>
-
+#include <stdio.h>
 /**
  * _isupper - checks if a character is upper
  * @c:  the character
- * Return: 1 if c is upper and c otherwise
+ * Return: 1 if c is upper and 0 otherwise
  */
 
 int _isupper(int c)
