dc6a3241aed95b2f211d885e7a382e48007b3474
root <ail>
2022-03-21 21:36:34 -0700

    is digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,12 +2,13 @@
 /**
  * _isgigit - if a number is a digit
  * @c :  the number
- *
  * Return: 0 if false and 1 if true
  */
 int _isdigit(int c)
 {
-	if (c % 10 != 0)
+	if (c == 0)
+		return (1);
+	else if (c % 10 != 0)
 		return (1);
 	else
 		return (0);
