f329a217cadfcc9fb1cf4e09ba865c1ed5b1463f
root <ail>
2022-03-24 20:00:59 -0700

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,11 @@ void more_numbers(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (num = 0; num <= 14; num++)
-			_putchar(num + '0');
+		{
+			if (num > 9)
+				_putchar((num / 10) +'0');
+			_putchar((num % 10) + '0');
+		}
 		_putchar('\n');
 	}
-
-	_putchar('\n');
 }
