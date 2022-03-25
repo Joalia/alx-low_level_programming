6d6da32dd4d1803ea4957bc39768fe57b93446b7
root <ail>
2022-03-25 05:19:26 -0700

    101-print_number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,26 +6,16 @@
 
 void print_number(int n)
 {
-	int quo, rest;
-	quo = n;
+	unsigned int num = n;
 
-	if (quo / 1000 != 0)
+	if (n < 0)
 	{
-		rest = quo % 1000; 
-		quo = quo / 1000;
-		_putchar(rest +'0');
+		_putchar('-');
+		num = -num;
 	}
-	if (quo / 100 != 0)
-	{
-		rest = quo % 100;
-		quo = quo / 100;
-		_putchar(rest + '0');
-	}
-	if (quo / 10 != 0)
-	{
-		rest = quo % 10;
-		quo = quo / 10;
-		_putchar(rest + '0');
-	}
-	_putchar(quo +'0');
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
