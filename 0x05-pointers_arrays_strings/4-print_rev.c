9ce813beb92a98eff5a5209b8eef0f8165fbb2b6
root <ail>
2022-03-25 23:56:51 -0700

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - print a string in reverse followed by a new line
+ * @s: the string
+ */
+void print_prev(char *s)
+{
+	int i, len;
+
+	len = _strlen(s);
+
+	for (i = len; i <= 0; i--)
+	{
+		printf("%c", *(s + i));
+	}
+	printf("\n");
+}
