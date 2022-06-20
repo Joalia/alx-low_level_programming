218cdf5737b6f79a54c2266beb43530239e8abc1
Joalia <annelisejulianeo@gmail.com>
2022-06-20 05:37:49 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,27 +5,20 @@
  * *_strncat - concatenate 2 string
  * @dest: the first
  * @src: the second string
- * @n: the bytes
+ * @n: the limit of the src strinc to be taken
  * Return: a string
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
-	int index1, index2 = 0, len1, len2, limit;
+        int index1, index2 = 0, len1, len2;
 
-	len1 = strlen(dest);
-	len2 = strlen(src);
+        len1 = strlen(dest);
+        len2 = strlen(src);
 
-	if (n = 0)
-		return (dest);
-	else if (n <= len2)
-		limit = len1 + n;
-	else
-		limit = len1 + len2;
-
-	for (index1 = len1; index1 < limit; index1++)
-	{
-		dest[index1] = src[index2];
-		index2++;
-	}
-	return (dest);
+        for (index1 = len1; index1 < len1 + len2; index1++)
+        {
+                dest[index1] = src[index2];
+                index2++;
+        }
+        return (dest);
 }
