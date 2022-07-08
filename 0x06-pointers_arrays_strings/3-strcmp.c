3d4e79ff1988b090a809ad8126fce0d547530ff7
Joalia <annelisejulianeo@gmail.com>
2022-07-08 02:47:53 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,27 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int tail;
+	int tail, i;
+	int len1 = 0;
+	int len2 = 0;
 	tail = strlen(s1) - strlen(s2);
 
 	if (tail == 0)
+	{
+		for (i = 0; i < strlen(s1); i++)
+		{
+			len1 = len1 + s1[i];
+			len2 = len2 + s2[i];
+		}
+
+		if (len1 > len2):
+			return 1;
+		if (len1 < len2)
+			return -1;
 		return 0;
+	}
+
+
 	else if (tail < 0)
 		return -1;
 	else
